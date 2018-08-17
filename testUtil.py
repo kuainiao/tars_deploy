9a1114d05a474e947e54eb0a7b92442fc6a2ed89
qq4080 <408019910@qq.com>
2018-08-17 20:01:39 +0800

    s

--- testUtil.py
+++ testUtil.py
@@ -2,12 +2,24 @@
 # encoding: utf-8
 import os
 import comm.tarsUtil as tarsUtil
+import subprocess
+import sys
+import time
+import random
+import re
 
 def getAbabsolutePath():
     path = os.path.abspath(os.path.join(os.getcwd(), "../.."))
     return path
 
+def doCom(cmd):
+    popen = subprocess.Popen(cmd, stdout = subprocess.PIPE)
+    while True:
+        print popen.stdout.readline()
+    return
+
+
 if __name__ == '__main__':
-    tarsUtil.doCmd("/data/tars_deploy/pout.sh")
+    doCmd("/data/tars_deploy/pout.sh")
     pass
 
