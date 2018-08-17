fce7cc0a91e7acc487a93e974c66cbdd8ead2f38
qq4080 <408019910@qq.com>
2018-08-17 20:32:06 +0800

    d

--- testUtil.py
+++ testUtil.py
@@ -13,9 +13,19 @@ def getAbabsolutePath():
     return path
 
 def doCmd(cmd):
-    popen = subprocess.Popen(cmd, stdout = subprocess.PIPE)
-    while True:
-        print popen.stdout.readline()
+    p = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    while p.poll() is None:
+        line = p.stdout.readline()
+        line = line.strip()
+        if line:
+            print "Subprogram output: [{}]".format(line)
+    if p.returncode == 0:
+        print "Subprogram success"
+    else:
+        print "Subprogram failed"
+    p.stdin.close()
+    p.stdout.close()
+    p.stderr.close()
     return
 
 
