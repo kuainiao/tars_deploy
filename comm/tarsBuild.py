a04a7595dad9d32067e729853bd0645afde8b81d
qq4080 <408019910@qq.com>
2018-08-17 20:54:23 +0800

    a

--- comm/tarsBuild.py
+++ comm/tarsBuild.py
@@ -1,18 +1,37 @@
 #!/usr/bin/python
 # encoding: utf-8
-
 import tarsLog
 import tarsUtil
+log = tarsLog.getLogger()
 def do():
     build()
     return
 
 def build():
     baseDir = getBaseDir()
-    doCmd("{}/cpp/build/build.sh cleanall".format(baseDir))
-    doCmd("{}/cpp/build/build.sh all".format(baseDir))
+    tarsUtil.doCmd("{}/cpp/build/build.sh cleanall".format(baseDir))
+    rCode = doCmd("{}/cpp/build/build.sh all".format(baseDir))
+    if rCode == 0:
+        log.info(" build success !!!")
+    else:
+        log.info(" build error !!!")
     return
 
+def doCmd(cmd):
+    rCode=0
+    p = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    while p.poll() is None:
+        line = p.stdout.readline()
+        line = line.strip()
+        if line:
+            log.info(line)
+    if p.returncode == 0:
+        rCode =0
+    else:
+        rCode=-1
+    p.stdin.close()
+    p.stdout.close()
+    return rCode
 
 if __name__ == '__main__':
     pass
