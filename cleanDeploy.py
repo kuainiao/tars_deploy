d1cbb968a58d548c24b24442547f6d1ab4f1134f
qq4080 <408019910@qq.com>
2018-08-19 18:47:55 +0800

    d

--- cleanDeploy.py
+++ cleanDeploy.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python
 # encoding: utf-8
 import comm.tarsLog as tarsLog
-import comm.tarsUtil as tarsUtil
+from comm.tarsUtil import *
 import shutil
 def do():
-    tarsUtil.doCmd("ps -aux|grep resin|grep -v grep|awk '{print \" kill -9\" ,$2}'|bash")
-    #shutil.rmtree("/usr/local/app")
+    doCmd("ps -aux|grep resin|grep -v grep|awk '{print \" kill -9\" ,$2}'|bash")
+    doCmd("ps -aux|grep tars|grep -v grep|awk '{print \" kill -9\" ,$2}'|bash")
+    doCmd("mysql -utars -ptars2015 -e 'drop database db_tars'")
+    doCmd("mysql -utars -ptars2015 -e 'drop database tars_stat'")
+    doCmd("mysql -utars -ptars2015 -e 'drop database tars_property'")
+    shutil.rmtree("/usr/local/app")
     return
 if __name__ == '__main__':
     do()
