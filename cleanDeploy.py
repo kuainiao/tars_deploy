ec983329b314012b7f5847f6998d190091b57ec2
qq4080 <408019910@qq.com>
2018-08-19 17:26:25 +0800

    d

--- cleanDeploy.py
+++ cleanDeploy.py
@@ -4,7 +4,7 @@ import comm.tarsLog as tarsLog
 import comm.tarsUtil as tarsUtil
 import shutil
 def do():
-    tarsUtil.doCmd("ps -aux|grep resin|awk '{print \" kill -9\" ,$2}'|bash")
+    tarsUtil.doCmd("ps -aux|grep resin|grep -v grep|awk '{print \" kill -9\" ,$2}'|bash")
     #shutil.rmtree("/usr/local/app")
     return
 if __name__ == '__main__':
