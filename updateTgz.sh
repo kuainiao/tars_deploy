7f8f4302f48110848a5af6fc786d3c4e3a3e0229
qq4080 <408019910@qq.com>
2018-08-23 11:02:16 +0800

    1

--- updateTgz.sh
+++ updateTgz.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-pushd /data/tars_deploy/ && git pull && popd
+#pushd /data/tars_deploy/ && git pull && popd
 #rm -rf /data/Tars/tars_deploy
 #cp -rf /data/tars_deploy/ /data/Tars/
 \cp -r /data/Tars_add/tars*/  /data/Tars/cpp/framework/deploy/
 \cp -rf /data/Tars_add/db/*  /data/Tars/cpp/framework/sql
 mkdir -p /usr/local/app/
-cp -rf /data/Tars_add/resin   /usr/local/app/
+rm -rf /data/Tars/web
+cp -rf /data/Tars_add/web /data/Tars/web
 
