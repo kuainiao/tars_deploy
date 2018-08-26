dcc27b957d263013d06d7adb4005977516eb4e47
qq4080 <408019910@qq.com>
2018-08-26 15:55:12 +0800

    1

--- comm/tarsTest.py
+++ comm/tarsTest.py
@@ -8,6 +8,7 @@ from tarsUtil import *
 log = tarsLog.getLogger()
 #localIp = getLocalIp()
 localIp = "172.16.0.17"
+webPort = "8080"
 def do():
     (rCode,msg) = testRegistry()
     if rCode !=0:
@@ -33,13 +34,16 @@ def testWeb():
     return testByInterface("/pages/tree","","sdfdddd")
 
 def testFrameServer():
+    frameServer = [("serverName":"tarspatch","serverId","1"),("serverName":"tarsconfig","serverId":"2")]
+    for (serverName,serverId) in frameServer:
+        print "test server {} start ".format(serverName)
     return (0,"")
 
 def testByInterface(uri,params,indexKey):
-    url ="http://{}:8080/{}".format(localIp,uri)
-    print url
+    url ="http://{}:{}/{}".format(localIp,webPort,uri)
+    log.infoPrint(url)
     result = requests.get(url,data=params)
-    print " statusCode is {} ,text is {} ".format(result.status_code,result.text)
+    log.infoPrint(" statusCode is {} ,text is {} ".format(result.status_code,result.text))
     if(result.status_code!=200):
         return (-1,"test fail,tarweb cannot visit")
     else:
