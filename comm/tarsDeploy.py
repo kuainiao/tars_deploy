3bed94e531759c4fecec5183325772f97fa46e41
qq4080 <408019910@qq.com>
2018-08-30 17:46:24 +0800

    1

--- comm/tarsDeploy.py
+++ comm/tarsDeploy.py
@@ -105,24 +105,24 @@ def deployNodeJsEnvironmentCentos():
     return
 
 def deployNodeJsEnvironmentSuse():
-    result = doCmdIgnoreException("source ~/.bashrc;nvm --version")
+    result = doCmdIgnoreException("nvm --version")
     if result["status"] != 0:
         log.infoPrint("install nvm start...")
         doCmd("wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash")
-        #os.system("export NVM_DIR=\"$HOME/.nvm\";[ -s \"$NVM_DIR/nvm.sh\" ] && \. \"$NVM_DIR/nvm.sh\";")
+        os.system("export NVM_DIR=\"$HOME/.nvm\";[ -s \"$NVM_DIR/nvm.sh\" ] && \. \"$NVM_DIR/nvm.sh\";")
         log.infoPrint("install nvm success")
     else:
         log.infoPrint("nvm version  is {}".format(result["output"]))
-    result = doCmdIgnoreException("source ~/.bashrc;node --version")
+    result = doCmdIgnoreException("node --version")
     if result["status"] != 0:
         log.infoPrint("install node start...")
-        doCmd("source ~/.bashrc;nvm install v8.11.3")
+        doCmd("nvm install v8.11.3")
         log.infoPrint("install node success")
     else:
         log.infoPrint("node version  is {}".format(result["output"]))
-    result = doCmdIgnoreException("source ~/.bashrc;pm2 --version")
+    result = doCmdIgnoreException("pm2 --version")
     if result["status"] != 0:
-        doCmd("source ~/.bashrc;npm install -g pm2 --registry=https://registry.npm.taobao.org")
+        doCmd("npm install -g pm2 --registry=https://registry.npm.taobao.org")
     else:
         log.infoPrint("pm2 version  is {}".format(result["output"]))
     return
