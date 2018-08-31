a24b96fe791413d73a3e444ad7c9c0dd7dc22091
qq4080 <408019910@qq.com>
2018-08-31 17:29:47 +0800

    1

--- deploy.py
+++ deploy.py
@@ -54,6 +54,7 @@ Parameter:
 Attention:
     1.support with python 2.7
     2.network unobstructed
+    3.you had installed :gcc,gcc-c++,cmake,yasm,glibc-devel,flex,bison,ncurses-devel,zlib-devel,autoconf ,if not ,we will install it ,but It's possible to fail.
     '''
     print helpMsg
     return
